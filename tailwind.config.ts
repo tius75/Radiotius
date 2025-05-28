a7d2c688f3fb3535d963dc4013dd1d9ded378d6f
tius75 <76941855+tius75@users.noreply.github.com>
2025-05-28 12:25:54 +0700

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,158 @@
+import { type Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import animate from "tailwindcss-animate";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+
+export default {
+	darkMode: ["class"],
+	content: ["./src/**/*.tsx"],
+	theme: {
+		opacity: {
+			'0': '0',
+			'10': '0.1',
+			'20': '0.2',
+			'30': '0.3',
+			'40': '0.4',
+			'50': '0.5',
+			'60': '0.6',
+			'70': '0.7',
+			'80': '0.8',
+			'90': '0.9',
+			'100': '1',
+		},
+		extend: {
+			fontFamily: {
+				sans: [
+					'var(--font-poppins)',
+					...defaultTheme.fontFamily.sans
+				]
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				card: {
+					DEFAULT: 'var(--card)',
+					foreground: 'var(--card-foreground)'
+				},
+				popover: {
+					DEFAULT: 'var(--popover)',
+					foreground: 'var(--popover-foreground)'
+				},
+				primary: {
+					DEFAULT: 'var(--primary)',
+					foreground: 'var(--primary-foreground)'
+				},
+				secondary: {
+					DEFAULT: 'var(--secondary)',
+					foreground: 'var(--secondary-foreground)'
+				},
+				muted: {
+					DEFAULT: 'var(--muted)',
+					foreground: 'var(--muted-foreground)'
+				},
+				accent: {
+					DEFAULT: 'var(--accent)',
+					foreground: 'var(--accent-foreground)'
+				},
+				destructive: {
+					DEFAULT: 'var(--destructive)',
+					foreground: 'var(--destructive-foreground)'
+				},
+				border: 'var(--border)',
+				input: 'var(--input)',
+				ring: 'var(--ring)',
+				chart: {
+					'1': 'var(--chart-1)',
+					'2': 'var(--chart-2)',
+					'3': 'var(--chart-3)',
+					'4': 'var(--chart-4)',
+					'5': 'var(--chart-5)'
+				},
+				sidebar: {
+					DEFAULT: 'var(--sidebar-background)',
+					foreground: 'var(--sidebar-foreground)',
+					primary: 'var(--sidebar-primary)',
+					'primary-foreground': 'var(--sidebar-primary-foreground)',
+					accent: 'var(--sidebar-accent)',
+					'accent-foreground': 'var(--sidebar-accent-foreground)',
+					border: 'var(--sidebar-border)',
+					ring: 'var(--sidebar-ring)'
+				}
+			},
+			keyframes: {
+				'accordion-down': {
+					from: {
+						height: '0'
+					},
+					to: {
+						height: 'var(--radix-accordion-content-height)'
+					}
+				},
+				'accordion-up': {
+					from: {
+						height: 'var(--radix-accordion-content-height)'
+					},
+					to: {
+						height: '0'
+					}
+				},
+				'music-bars': {
+					'0%, 100%': { height: '0.75rem' },
+					'50%': { height: '1.75rem' }
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'music-bars': 'music-bars 0.7s ease-in-out infinite'
+			},
+			utilities: {
+				'.animation-delay-200': {
+					'animation-delay': '0.2s',
+				},
+				'.animation-delay-400': {
+					'animation-delay': '0.4s',
+				},
+			},
+		}
+	},
+	plugins: [
+		animate,
+		addVariablesForColors,
+		function({ addUtilities }: any) {
+			const newUtilities = {
+				'.animation-delay-200': {
+					'animation-delay': '0.2s',
+				},
+				'.animation-delay-400': {
+					'animation-delay': '0.4s',
+				},
+				'.scrollbar-hide': {
+					'-ms-overflow-style': 'none',
+					'scrollbar-width': 'none',
+					'&::-webkit-scrollbar': {
+						display: 'none',
+					},
+				},
+			}
+			addUtilities(newUtilities)
+		},
+	],
+} satisfies Config;
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+
+	addBase({
+		":root": newVars,
+	});
+}
